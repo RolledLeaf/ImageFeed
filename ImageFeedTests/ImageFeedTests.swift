aca46d53a0fe9f91b93278265497d5cf3a8e9ac3
Prototype90 <mrkupukupu@gmail.com>
2024-09-28 14:09:49 +0300

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Vitaly Wexler on 28.09.2024.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
