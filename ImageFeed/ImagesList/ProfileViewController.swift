9f035f4c4e60a416431102cfcbdace176600c7ed
Vitaly Wexler <mrkupukupu@gmail.com>
2024-10-13 14:22:10 +0300

    Вёрстка страницы профиля

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileID: UILabel!
+    @IBOutlet weak var profileDescription: UILabel!
+    
+    
+    @IBAction func logoutButton(_ sender: Any) {
+    }
+    
+
+    @IBAction func profileButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func menuButton(_ sender: Any) {
+    }
+}
