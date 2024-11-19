569c787cf0fe517b6ffe04d6b3d6467edb5a2b0c
Vitaly Wexler <mrkupukupu@gmail.com>
2024-11-19 17:27:57 +0300

    реализация сохранения токенов в keyChain

--- ImageFeed/Services/OAuth2TokenStorage.swift
+++ ImageFeed/Services/OAuth2TokenStorage.swift
@@ -1,32 +1,37 @@
 import Foundation
 import WebKit
+import SwiftKeychainWrapper
 
-//Упраление хранением токена
+// Управление хранением токена
 final class OAuth2TokenStorage {
     static let shared = OAuth2TokenStorage()
-    init() {}
-    
     private let tokenKey = "oauth_token"
-    
+
+     init() {}
+
     var token: String? {
         get {
-            return UserDefaults.standard.string(forKey: tokenKey)
+            return KeychainWrapper.standard.string(forKey: tokenKey)
         }
         set {
-            UserDefaults.standard.setValue(newValue, forKey: tokenKey)
-            print("Token saved: \(newValue ?? "")")
+            if let newValue = newValue {
+                let success = KeychainWrapper.standard.set(newValue, forKey: tokenKey)
+                print("Token saved successfully to keychain: \(success)")
+            } else {
+                let success = KeychainWrapper.standard.removeObject(forKey: tokenKey)
+                print("Token removed from keychain successfully: \(success)")
+            }
         }
-        
     }
-    
+
     func clearToken() {
-        UserDefaults.standard.removeObject(forKey: tokenKey)
-        print("Token removed: \(tokenKey)")
+        let success = KeychainWrapper.standard.removeObject(forKey: tokenKey)
+        print("Token removed from keychain with clearToken method successfully: \(success)")
     }
-    
+
     func clearCookies() {
         let cookieStore = WKWebsiteDataStore.default().httpCookieStore
-        cookieStore.getAllCookies() { cookies in
+        cookieStore.getAllCookies { cookies in
             for cookie in cookies {
                 cookieStore.delete(cookie)
             }
